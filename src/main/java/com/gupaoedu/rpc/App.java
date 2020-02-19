feba99c8f91f0a904cbdc557e6c5959139a4ce2e
panning255 <panning255@jk.cn>
2020-02-19 21:24:09 +0800

    Initial commit

---
+++ src/main/java/com/gupaoedu/rpc/App.java
@@ -0,0 +1,20 @@
+package com.gupaoedu.rpc;
+
+import com.gupaoedu.vip.IPaymentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new
+                AnnotationConfigApplicationContext(SpringConfig.class);
+        RpcProxyClient rpcProxyClient = context.getBean(RpcProxyClient.class);
+        IPaymentService iPaymentService = rpcProxyClient.clientProxy(IPaymentService.class,
+                "localhost", 8080);
+
+        iPaymentService.doPay();
+    }
+}
---
+++ src/main/java/com/gupaoedu/rpc/SpringConfig.java
@@ -0,0 +1,13 @@
+package com.gupaoedu.rpc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean(name = "rpcProxyClient")
+    public RpcProxyClient proxyClient() {
+        return new RpcProxyClient();
+    }
+}
